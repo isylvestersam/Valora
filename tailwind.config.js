42ed3638ac337fcf3a963b30869674b8a3a2900a
isylsam <isylvestersam@gmail.com>
2025-08-19 11:28:54 +0100

    Added landing page and new assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,15 @@ module.exports = {
         raleway: "Raleway"
       },
       colors: {
-        primary: 'rgb(7,61,68)',
-        lighter: 'rgb(56,93,101)',
+        primary: 'rgb(50,0,83)',
+        lighter: 'rgb(217,193,255)',
+        lightest: "rgb(230,224,246)",
+        lightest2: "rgb(85,38,120)",
+        milk: "rgb(247,246,249)",
+        box1: "rgb(241,241,243)",
+        box2: "rgb(216,193,254)",
+        box3: "rgb(rgb(241,238,244)",
+        bpurple: "rgb(83,41,115)",
       }
     },
   },
